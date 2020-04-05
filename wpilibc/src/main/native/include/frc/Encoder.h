63f13f24172e9a3e0429c3da7327caa4279e7e65
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2020-04-05 10:36:06 +0300

    fix memory import

--- wpilibc/src/main/native/include/frc/Encoder.h
+++ wpilibc/src/main/native/include/frc/Encoder.h
@@ -6,7 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 #pragma once
-#include <memory.h>
+#include <memory>
 #include <wpi/deprecated.h>
 #include "QuadratureEncoder.h"
 
